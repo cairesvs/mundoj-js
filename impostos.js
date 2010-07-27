bd90ce2fbd14f061b47f081b15d7cb2340622661
Caires Vinicius <cairesvs@gmail.com>
2010-07-26 23:33:19 -0300

    atualizando codigos com o da materia

--- impostos.js
+++ impostos.js
@@ -1,9 +1,10 @@
 var Imposto = {};
 
 Imposto.calcula = function(valor, imposto, taxa){
-	var valorInteiro = valor.split(",")[0];
-	var valorDecimal = valor.split(",")[1];
-	var real = parseFloat((valorInteiro + "." + valorDecimal)).toFixed(2);
-	var formatted = ((real * imposto) + taxa).toString().replace(".",",");
-	return formatted;
+	return ((valor * imposto) + taxa);
+}
+
+Imposto.bigNumber = function(valor, imposto, taxa){
+	var valorComImposto = new BigNumber(valor).multiply(imposto);
+	return Number(new BigNumber(taxa).add(valorComImposto));
 }
